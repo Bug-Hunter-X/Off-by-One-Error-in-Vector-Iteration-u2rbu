8388f37fb957d49002154cd977bc03a269ccb13b
BugHunter-X <askzenai@gmail.com>
2025-01-24 22:44:05 +0100

    Add bugSolution.cpp file

---
+++ bugSolution.cpp
@@ -0,0 +1,7 @@
+std::vector<int> vec = {1, 2, 3, 4, 5};
+
+for (int i = 0; i < vec.size(); ++i) { //Corrected: Loop until i is strictly less than vec.size()
+  std::cout << vec[i] << " ";
+}
+
+std::cout << "\n";
